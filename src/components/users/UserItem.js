7d4dfe22c68ba1e3c91c12c94a1ae2359ade5e93
azorji kelechi oliver <kelechioliver96@gmail.com>
2019-06-12 23:07:42 +0100

    #5577489 platform alignment

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 const UserItem = ({ user: { avatar_url, login, html_url } }) => {
+	let displayName;
+	if(login.length > 5){
+		displayName = `${login.slice(0,5)}...`	
+		displayName[0].toUpperCase();
+	}else{
+		displayName = login
+		displayName[0].toUpperCase();
+	}
 	return (
 		<div className="card text-center">
 			<img src={avatar_url} alt="user" className="round-img" style={{ width: '60px' }} />
-			<h3>{login}</h3>
+			<h3>{displayName}</h3>
 			<div>
 				<Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
 					More..
